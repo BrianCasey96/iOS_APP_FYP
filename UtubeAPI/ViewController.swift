99f5ab6a816b5d20e161e3ac28fe65ff4fd68852
Casey, Brian <i330151@dubm60265081a.local>
2017-01-12 15:43:42 +0000

    Initial Commit

---
+++ UtubeAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UtubeAPI
+//
+//  Created by Casey, Brian on 12/01/2017.
+//  Copyright © 2017 Casey, Brian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
